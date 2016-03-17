0ce244bf09988f6b5e66610193559cedf4bd7197
Will Hayes <imwillhayes@gmail.com>
2016-03-17 14:42:07 +0800

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var project = "app"
 var gulp = require('gulp');
 var express = require('express');
-var sass=require('gulp-sass');
+var sass = require('gulp-sass');
 var bs = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('express', function() {
@@ -20,19 +22,35 @@ gulp.task('lr', function() {
 })
 
 gulp.task('styles', function() {
-  return gulp.src(project + '/sass/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulp.dest(project + '/css/'))
+	return gulp.src(project + '/sass/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		.pipe(gulp.dest(project + '/css/'))
 });
+
+gulp.task('imagemin', function() {
+	return gulp.src(project + '/images/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}, {
+				cleanupIDs: false
+			}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest(project + '/images'));
+})
 
 gulp.task('watch', function() {
-  gulp.watch(project + '/sass/*.scss', ['styles']);
-  gulp.watch(project + '/css/*.css',function(){bs.reload(project + '/css/*.css')});
-  gulp.watch(project + '/*.html', bs.reload);
-  gulp.watch(project + '/js/*.js',bs.reload);
+	gulp.watch(project + '/sass/*.scss', ['styles']);
+	gulp.watch(project + '/css/*.css', function() {
+		bs.reload(project + '/css/*.css')
+	});
+	gulp.watch(project + '/*.html', bs.reload);
+	gulp.watch(project + '/js/*.js', bs.reload);
 });
 
 
 
-gulp.task('default', ['express','styles','watch', 'lr'])
+gulp.task('default', ['express', 'styles', 'watch', 'lr', 'imagemin'])
